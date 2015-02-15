945504770af6a69d9d9819d95684697b6e4e86e1
smuda <john@ayoy.se>
2015-02-15 11:31:49 +0100

    Added project skeleton.

---
+++ ChangePictureName/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace ChangePictureName
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                return;
+            }
+        }
+    }
+}
